e5eba55682259c74ba6b00af1a9e8d39f01a7cd4
Tariq <tariq_esa90@yahoo.com>
2023-06-09 01:58:15 +0100

    added 3d canvas and a sphere to home section

--- src/components/3d/ThreeD.jsx
+++ src/components/3d/ThreeD.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame,useLoader } from '@react-three/fiber'
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from 'react'
 import './3d.css'
-import * as THREE from 'three';
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
 
 function ThreeD() {
   return (
-    <div>ThreeD</div>
+    <div className="threeD__container">
+    <Canvas id='threeD__container'>
+    <Suspense fallback={null}>
+          <Model id='model'/>
+          <OrbitControls />
+          <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 5, 5]} />
+    </Suspense>
+    </Canvas>
+    </div>
   )
 }
+
+// const MyMesh = () => {
+//     const ref = useRef()
+//     useFrame((state, delta) => (ref.current.rotation.x += delta))
+//     const [hovered, hover] = useState(false)
+//     const [clicked, click] = useState(false)
+//     return (  
+        
+//         <mesh
+//         ref={ref}
+//         scale={clicked ? 1.5 : 1}
+//         onClick={(event) => click(!clicked)}
+//         onPointerOver={(event) => hover(true)}
+//         onPointerOut={(event) => hover(false)}>
+//         <boxGeometry args={[1, 1, 1]} />
+//         <meshStandardMaterial color={hovered ? 'white': 'white'} />
+//       </mesh>);
+//   }
+
+
+  const Model = () => {
+    const ref = useRef()
+    useFrame((state, delta) => (ref.current.rotation.x += delta))
+    const gltf = useLoader(GLTFLoader, "./sphere.gltf");
+    return (
+      <>
+        <primitive object={gltf.scene} scale={2} ref={ref} />
+      </>
+    );
+  };
+
 
 export default ThreeD
